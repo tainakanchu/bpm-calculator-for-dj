b8f3472fcd64666694f3d04a4cb35ba8226bc610
tainakanchu <tainakanchu815@gmail.com>
2023-07-28 07:44:39 +0900

    オフラインでキャッシュするように修正

--- next.config.js
+++ next.config.js
@@ -3,6 +3,31 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "google-fonts",
+        expiration: {
+          maxEntries: 4,
+          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/bpm-calculator-for-dj\.vercel.app\/*/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "contents",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+        },
+        networkTimeoutSeconds: 10,
+      },
+    },
+  ],
 });
 
 module.exports = withPWA({
