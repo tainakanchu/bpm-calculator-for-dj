69a77cb4913167bef19aa3dba14b108431b39991
tainakanchu <tainakanchu815@gmail.com>
2023-08-04 12:11:01 +0900

    add meta info

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,55 @@ const bizUd = BIZ_UDPGothic({
   subsets: ["latin"],
 });
 
+const title = "BPM Calculator";
+const name = "BPM Calculator for DJs";
+const description = "Intelligent BPM Calculator for DJs";
+const abstract =
+  "Intelligent BPM Calculator for DJs. BPM is calculated from the result of multiple taps of the button. It also displays the result of the division set based on the result.";
+const url = "https://bpm-calculator.vercel.app/";
+
+const ogImagePath = "./image/ogp.png";
+
 export const metadata: Metadata = {
-  title: "BPM Calculator",
-  description: "Intelligent BPM Calculator for DJs",
+  title,
+  description,
+  applicationName: name,
+  authors: [
+    {
+      url: "https://github.com/tainakanchu",
+      name: "tainakanchu",
+    },
+  ],
+  keywords: [
+    "BPM",
+    "DJ",
+    "BPM Calculator",
+    "BPM計算",
+    "BPM計算機",
+    "BPM計算アプリ",
+    "ポリリズム",
+  ],
+  themeColor: "#080808",
+  colorScheme: "dark",
+  icons: ["./icon.png"],
+  abstract,
+  openGraph: {
+    title,
+    siteName: name,
+    type: "website",
+    images: [ogImagePath],
+    description,
+    locale: "ja_JP",
+    url: url,
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: url,
+    title,
+    description,
+    images: [ogImagePath],
+  },
+  appleWebApp: true,
 };
 
 export default function RootLayout({
@@ -27,6 +73,10 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/icon.png"></link>
         <link rel="icon" href="/favicon.png"></link>
         <meta name="theme-color" content="#080808" />
+        <meta
+          name="google-site-verification"
+          content="JwWIxoPltJpLgYWfsb-bRZa5r5_DrvCg_IeBAATCJo0"
+        />
       </head>
       <body className={bizUd.className}>
         {children}
